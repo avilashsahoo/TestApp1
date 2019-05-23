0944420f5f9aaf67be1d39f11c042aeb5b782f40
Avilash Sahoo (asahoo) <Avilash.Sahoo@quest.com>
2019-05-23 12:24:18 +0530

    First Checkin

---
+++ TestProject/UnitTest1.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace TestProject
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            IWebDriver driver = new ChromeDriver(@"C:\Automation\");
+            driver.Navigate().GoToUrl("https://google.com");
+        }
+    }
+}
